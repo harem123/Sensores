a6b2db4bf28e2474b6d8d852033f11b865c6d533
harem123 <31325055+harem123@users.noreply.github.com>
2017-09-12 15:46:23 -0500

    logrado sensa 3 pines por orden serial
    sensa 3 pines sale correctamente de el while, se usa con la orden C1Z, falta agregar un t=10

--- Arduino_RS485_Esclavos/Arduino_RS485_Esclavos.ino
+++ Arduino_RS485_Esclavos/Arduino_RS485_Esclavos.ino
@@ -4,13 +4,23 @@ const int En_WrRd_RS485 =  2;
 const int Led_1 =  13; 
 const int Led_2 =  6; 
 const int Led_3 =  5;  
-
+// ++ pines de sensado my pins y pin de salida 
+int myPins[] = {7, 8,9};
+int sN=33;
+boolean flag;
+// ++++++++++++++++++++++++++++++
+// variables de buffering
 char VarChar = ' ';
 String BufferIn = "";        
 boolean StringCompleta = false; 
 
 void setup() 
 { 
+  //++ defino pines de entrada 
+  for(int j= 0 ; j <2; j++){
+  pinMode(myPins[j],INPUT);}
+// +++
+
   Serial.begin(9600);
   BufferIn.reserve(5);  
     
@@ -34,19 +44,27 @@ void loop()
        digitalWrite(En_WrRd_RS485, LOW); 
       Serial.print(BufferIn);
       
-      //if ((BufferIn.indexOf('B')) >= 0)
+      
       if ((BufferIn.indexOf('C')) >= 0)
       {
           if (BufferIn.indexOf('1' ) >= 0)
           {
+            flag = true;
             Serial.print("OK");
             delay(500);
-            
-              
-                  
+            while(flag == true)
+            {
               digitalWrite(Led_2, HIGH);
-               
-             
+              for(int i = 0; i<3; i++)
+              {
+                if (digitalRead(myPins[i]) == HIGH)
+                {
+                  sN = i;
+                  Serial.print(sN);
+                  flag = false;
+                }
+              }
+            }
              
             
             delay(500);
